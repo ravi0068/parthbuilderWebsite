b355f1596d666708a8068b136e195fe3fbcf9f17
fantasyscore <164643183+fantasyscore@users.noreply.github.com>
2025-02-06 12:41:55 +0530

    website all changes done , made responsive on all screen

--- js/custome.js
+++ js/custome.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
           {
               breakpoint: 1024,
               settings: {
-                  slidesToShow: 4,
+                  slidesToShow: 3,
                   slidesToScroll: 1,
               }
           },
           {
               breakpoint: 600,
               settings: {
-                  slidesToShow: 3,
+                  slidesToShow: 2,
                   slidesToScroll: 1,
                   arrows: false
               }
@@ -26,7 +26,7 @@ $(document).ready(function () {
           {
               breakpoint: 480,
               settings: {
-                  slidesToShow: 2,
+                  slidesToShow: 1,
                   slidesToScroll: 1
               }
           }
